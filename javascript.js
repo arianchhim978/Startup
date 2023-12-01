ad7faf279b58182b2eb03e4f348a6e8a5b0287b9
arianchhim978 <arianmchhim@gmail.com>
2023-12-01 17:48:35 -0500

    added photo carousel

---
+++ javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+  $('.carousel').slick({
+      autoplay: true,
+      autoplaySpeed: 2000, // Adjust the speed (in milliseconds) as needed
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      fade: true,
+      cssEase: 'linear'
+  });
+});
