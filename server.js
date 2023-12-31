b651f88cf222e3cad02b671459ab8f5c8bd82dcc
arianchhim978 <147645634+arianchhim978@users.noreply.github.com>
2023-12-30 21:12:38 -0500

    refreshed

--- server.js
+++ server.js
@@ -3,37 +3,37 @@ const bodyParser = require('body-parser');
 const twilio = require('twilio');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
-// Parse incoming requests with JSON payloads
-app.use(bodyParser.json());
-
-// Initialize Twilio with your credentials
+// Replace these values with your Twilio credentials
 const accountSid = 'ACccc8e7776a399f5d9a4fedc6046b71b5';
 const authToken = 'd0decb3f8a97952dcf03b9ad5bb8cca4';
+const twilioPhoneNumber = '+18556271276';
+
 const client = twilio(accountSid, authToken);
 
-// Define your route for handling RSVP submissions
-app.post('/submit-rsvp', (req, res) => {
-    // Extract data from the request
-    const { name, phoneNumber } = req.body;
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
-    // Your logic to save RSVP data to Firebase or other database
+app.post('/send-rsvp', (req, res) => {
+    const { phoneNumber, message } = req.body;
 
-    // Send SMS using Twilio
-    client.messages.create({
-        body: `Thank you for your RSVP, ${name}!`,
-        from: '+18556271276',
-        to: phoneNumber,
-    })
-    .then(message => console.log(message.sid))
-    .catch(error => console.error(error));
-
-    // Respond to the client
-    res.status(200).send('RSVP submitted successfully!');
+    client.messages
+        .create({
+            body: message,
+            from: twilioPhoneNumber,
+            to: phoneNumber,
+        })
+        .then((message) => {
+            console.log('RSVP message sent:', message.sid);
+            res.status(200).send('RSVP message sent successfully.');
+        })
+        .catch((error) => {
+            console.error('Error sending RSVP:', error);
+            res.status(500).send(`Error sending RSVP: ${error.message}`);
+        });
 });
 
-// Start the server
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
